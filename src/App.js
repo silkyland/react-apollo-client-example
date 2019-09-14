0152d80ece6617a411e5878fd05522ede26a2d81
Bundit Nuntates <silkyland@gmail.com>
2019-09-14 15:30:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { GET_USERS } from "./client/query/user";
-import { Query } from "@apollo/react-components";
-import Skeleton from "react-loading-skeleton";
-
-function App() {
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import User from "./components/admin/User";
+const App = () => {
   return (
-    <Query query={GET_USERS}>
-      {({ loading, error, data }) => {
-        if (loading) return <Skeleton count={5} />;
-        if (error) return <p>{error.message}</p>;
-        const { users } = data;
-        return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <ul>
-                {users.map(user => (
-                  <li key={user.id}>
-                    {user.name} || {user.email}
-                  </li>
-                ))}
-              </ul>
-            </header>
-          </div>
-        );
-      }}
-    </Query>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={User} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/admin/User/index.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Query } from "@apollo/react-components";
+import Skeleton from "react-loading-skeleton";
+import { GET_USERS } from "../../../client/query/user.js";
+import {
+  Container,
+  Card,
+  CardHeader,
+  CardBody,
+  Table,
+  Button,
+  ButtonGroup,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+
+const User = props => {
+  const [input, setInput] = useState({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: ""
+  });
+  return (
+    <Query query={GET_USERS}>
+      {({ loading, error, data }) => {
+        if (loading) return <Skeleton count={5} />;
+        if (error) return <p>{error.message}</p>;
+        const { users } = data;
+        return (
+          <div className="container">
+            <Card className="mt-5">
+              <CardHeader>
+                รายชื่อ
+                <span className="float-right">
+                  <Button size="sm" color="success">
+                    เพิ่ม
+                  </Button>
+                </span>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  <FormGroup>
+                    <Label>ชื่อผู้ใช้: </Label>
+                    <Input
+                      type="text"
+                      name="username"
+                      required
+                      placeholder="กรอกชื่อผู้ใช้"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>ชื่อ-สกุล: </Label>
+                    <Input
+                      type="text"
+                      name="name"
+                      required
+                      placeholder="กรอกชื่อผู้ใช้"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>อีเมล์: </Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      required
+                      placeholder="กรอกอีเมล์"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>พาสเวิร์ด: </Label>
+                    <Input
+                      type="password"
+                      name="password"
+                      required
+                      placeholder="กรอกพาสเวิร์ด"
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>พาสเวิร์ด: </Label>
+                    <Input
+                      type="password"
+                      name="password_confirmation"
+                      required
+                      placeholder="กรอกพาสเวิร์ดอีกครั้ง"
+                    />
+                  </FormGroup>
+                  <Button type="submit" color="primary">
+                    บันทึก
+                  </Button>
+                </Form>
+              </CardBody>
+              <Table striped hover bordered responsive>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>ขื่อ-สกุล</th>
+                    <th>ชื่อผู้ใช้</th>
+                    <th>อีเมล์</th>
+                    <th>จัดการ</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user, index) => (
+                    <tr key={user.id}>
+                      <td>{index + 1}</td>
+                      <td>{user.name}</td>
+                      <td>{user.username}</td>
+                      <td>{user.email}</td>
+                      <td>
+                        <ButtonGroup>
+                          <Button size="sm" color="warning" onClick={() => {}}>
+                            แก้ไข
+                          </Button>
+                          <Button size="sm" color="danger">
+                            ลบ
+                          </Button>
+                        </ButtonGroup>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Card>
+          </div>
+        );
+      }}
+    </Query>
+  );
+};
+
+export default User;
